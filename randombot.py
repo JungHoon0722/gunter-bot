ff5e01466f500a6293d20ff11b4a44e1dcd38af0
JungHoon0722 <88128762+JungHoon0722@users.noreply.github.com>
2022-04-13 00:08:54 +0900

    Update randombot.py

--- randombot.py
+++ randombot.py
@@ -30,6 +30,18 @@ async def on_message(message):
     if message.content == "김범준":
         await message.channel.send("ㅂ신")
         
+    if message.content == "그?":
+        asd = random.randint(0,1,2,3)
+        if asd == 0:
+            answer = "https://namu.wiki/w/%EB%B0%B0%EB%A6%AC%EB%82%98"
+        if asd == 1:
+            answer = "https://dimg.donga.com/wps/NEWS/IMAGE/2019/05/28/95731525.2.jpg"
+        if asd == 2:
+            answer = "https://img.asiatoday.co.kr/file/2019y/05m/27d/20190527001831462_1558950087_1.jpg"
+        if asd == 3:
+            answer = "https://mblogthumb-phinf.pstatic.net/MjAxOTA1MzBfMTQy/MDAxNTU5MTk4MDAyOTA3.B2sAcEWmJNCT1E7DN7eyR6KYa53CEkGzm96CJIFaEsQg.T5FmuoZfZJHjzWzmtf7c8LAILIf1JQBgs9nFodZAVXkg.JPEG.wjlisaju/%EC%BA%A1%EC%B2%98.JPG?type=w800"
+ 
+        
     if message.content == "bye":
         await message.channel.send("비와이")
         
